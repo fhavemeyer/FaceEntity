170ba058ceed9f42afb4a0d93c7d584c2959fcae
Frederick Havemeyer <frederick.havemeyer@gmail.com>
2013-12-18 12:18:18 -0500

    Setup channel logic

--- src/com/FaceEntity/ChannelListener.java
+++ src/com/FaceEntity/ChannelListener.java
@@ -1,11 +1,20 @@
 package com.FaceEntity;
 
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 import org.bukkit.entity.Player;
 
 public class ChannelListener implements PluginMessageListener {
+	private static final Logger log_ = Logger.getLogger("FaceEntity");
+	
+	public static void info(String message) {
+		log_.info("[FaceEntity] " + message);
+	}
+	
 	@Override
 	public void onPluginMessageReceived(String channel, Player player, byte[] message) {
-		
+		info("Received message on channel: " + channel);
 	}
 }
--- src/com/FaceEntity/FaceEntity.java
+++ src/com/FaceEntity/FaceEntity.java
@@ -3,16 +3,32 @@ package com.FaceEntity;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 
 public class FaceEntity extends JavaPlugin {
+	private final ChannelListener chanListener = new ChannelListener();
+	private final String CHANNEL_NAME = "faceentity";
+	
 	@Override
 	public void onEnable() {
-		
+		getLogger().info("Registering packet channels.");
+		Bukkit.getMessenger().registerOutgoingPluginChannel(this, this.CHANNEL_NAME);
+		Bukkit.getMessenger().registerIncomingPluginChannel(this, this.CHANNEL_NAME, this.chanListener);
 	}
 	
 	@Override
 	public void onDisable() {
 		
+	}
+	
+	@EventHandler
+	public void onPlayerJoinEvent(final Player playerJoined, final String joinMessage) {
+		if (playerJoined != null) {
+			String data = "thisdoesntmatter";
+			playerJoined.sendPluginMessage(this, this.CHANNEL_NAME, data.getBytes());
+			getLogger().info("Sent message on channel " + this.CHANNEL_NAME);
+		}
 	}
 }
