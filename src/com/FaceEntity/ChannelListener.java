e01944a1ee2d709b84d779843353d5702296d66b
Frederick Havemeyer <frederick.havemeyer@gmail.com>
2013-12-18 11:58:30 -0500

    Initial commit

---
+++ src/com/FaceEntity/ChannelListener.java
@@ -0,0 +1,11 @@
+package com.FaceEntity;
+
+import org.bukkit.plugin.messaging.PluginMessageListener;
+import org.bukkit.entity.Player;
+
+public class ChannelListener implements PluginMessageListener {
+	@Override
+	public void onPluginMessageReceived(String channel, Player player, byte[] message) {
+		
+	}
+}
---
+++ src/com/FaceEntity/FaceEntity.java
@@ -0,0 +1,18 @@
+package com.FaceEntity;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class FaceEntity extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
