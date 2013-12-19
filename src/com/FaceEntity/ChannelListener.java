46109d0083739e1f06f3d464bc403a7d8c90de52
Frederick Havemeyer <frederick.havemeyer@gmail.com>
2013-12-19 01:44:04 -0500

    Got this darn thing working

--- src/com/FaceEntity/ChannelListener.java
+++ src/com/FaceEntity/ChannelListener.java
@@ -1,5 +1,6 @@
 package com.FaceEntity;
 
+import java.io.UnsupportedEncodingException;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
@@ -7,14 +8,23 @@ import org.bukkit.plugin.messaging.PluginMessageListener;
 import org.bukkit.entity.Player;
 
 public class ChannelListener implements PluginMessageListener {
-	private static final Logger log_ = Logger.getLogger("FaceEntity");
+	private FaceEntity parent;
 	
-	public static void info(String message) {
-		log_.info("[FaceEntity] " + message);
+	public ChannelListener(FaceEntity f) {
+		super();
+		this.parent = f;
 	}
 	
 	@Override
 	public void onPluginMessageReceived(String channel, Player player, byte[] message) {
-		info("Received message on channel: " + channel);
+		this.parent.getLogger().info("Received message on channel: " + channel);
+		if (message != null && message.length > 0) {
+			try {
+				this.parent.getLogger().info("Loaded mods: " + (new String(message, "UTF-8")));
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
--- src/com/FaceEntity/FaceEntity.java
+++ src/com/FaceEntity/FaceEntity.java
@@ -1,34 +1,60 @@
 package com.FaceEntity;
 
-import org.bukkit.Bukkit;
+import java.lang.reflect.InvocationTargetException;
+
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import com.comphenix.protocol.*;
+import com.comphenix.protocol.events.PacketContainer;
 
-public class FaceEntity extends JavaPlugin {
-	private final ChannelListener chanListener = new ChannelListener();
+public class FaceEntity extends JavaPlugin implements Listener {
+	private ChannelListener channelListener;
 	private final String CHANNEL_NAME = "faceentity";
+	
+	private ProtocolManager protocolManager;
+	
+	@Override
+	public void onLoad() {
+		protocolManager = ProtocolLibrary.getProtocolManager();
+	}
 	
 	@Override
 	public void onEnable() {
+		this.channelListener = new ChannelListener(this);
+		
+		getServer().getPluginManager().registerEvents(this, this);
+		
 		getLogger().info("Registering packet channels.");
-		Bukkit.getMessenger().registerOutgoingPluginChannel(this, this.CHANNEL_NAME);
-		Bukkit.getMessenger().registerIncomingPluginChannel(this, this.CHANNEL_NAME, this.chanListener);
+		getServer().getMessenger().registerIncomingPluginChannel(this, this.CHANNEL_NAME, this.channelListener);
 	}
 	
 	@Override
 	public void onDisable() {
-		
+		getServer().getMessenger().unregisterIncomingPluginChannel(this, this.CHANNEL_NAME, this.channelListener);
 	}
 	
-	@EventHandler
-	public void onPlayerJoinEvent(final Player playerJoined, final String joinMessage) {
-		if (playerJoined != null) {
-			String data = "thisdoesntmatter";
-			playerJoined.sendPluginMessage(this, this.CHANNEL_NAME, data.getBytes());
-			getLogger().info("Sent message on channel " + this.CHANNEL_NAME);
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		final Player playerJoined = e.getPlayer();
+		if (playerJoined != null && playerJoined.isOnline()) { // Do I need these checks? I don't know...
+			this.sendCustomQueryPacket(playerJoined);
+		}
+	}
+	
+	private void sendCustomQueryPacket(Player p) {
+		PacketContainer queryPacket = this.protocolManager.createPacket(PacketType.Play.Server.CUSTOM_PAYLOAD);
+		queryPacket.getSpecificModifier(String.class).write(0, this.CHANNEL_NAME);
+		
+		try {
+			this.protocolManager.sendServerPacket(p, queryPacket);
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
